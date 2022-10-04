0d27ab06424341fd9d4a53bd2ca8704fe475e959
Mercad0 <abimaelmercado05@gmail.com>
2022-10-04 00:04:00 +0000

    the excuses here

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let myPotato = [
+    "baked potato",
+    "fried chickens",
+    "pollo tropical eggs",
+    "the neighborhood dog",
+    "the cat",
+    "the car"
+  ];
+  let ate = ["pushed", "slapped", "kicked", "threw "];
+  let theFork = [
+    "the rock to a lake",
+    "the spoon inside an oven",
+    "  the computer inside the toilet",
+    "the backpack onto the curb",
+    "the pencil on a notebook",
+    "released a bonus song",
+    "and dipped"
+  ];
+
+  function dog() {
+    document.getElementById("generatexcuse").innerHTML =
+      myPotato[Math.floor(Math.random() * myPotato.length)] +
+      " " +
+      ate[Math.floor(Math.random() * ate.length)] +
+      " " +
+      theFork[Math.floor(Math.random() * theFork.length)];
+  }
+  document.getElementById("btn").addEventListener("click", dog);
 };
